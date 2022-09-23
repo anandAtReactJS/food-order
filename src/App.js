148132b58bdffd9b06f4ed531c69510c564bbf99
Nivedita-Kodli <98208389+Nivedita-Kodli@users.noreply.github.com>
2022-09-23 13:39:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import Header from './component/Header';
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/homepage/HomePage';
+
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <BrowserRouter>
-        <Routes>
-            <Route path={'/'} element={<HomePage/>}/>
-        </Routes>
-      </BrowserRouter>
+      <Header/>
     </div>
   );
 }
--- src/component/Header.js
+++ src/component/Header.js
@@ -1,45 +1,33 @@
 import React from 'react'
+import "./Header.css"
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+
 
 const Header = () => {
   return (
-    <div className='container'>
-      <nav className="navbar navbar-expand-lg bg-light">
-        <div className="container-fluid">
-          <a className="navbar-brand" to="#">Navbar scroll</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarScroll">
-            <ul className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" >
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Link</a>
-              </li>
-              <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Link
-                </a>
-                <ul className="dropdown-menu">
-                  <li><a className="dropdown-item" to="#">Action</a></li>
-                  <li><a className="dropdown-item" href="#">Another action</a></li>
+    <nav className="navbar">
+      <div className="logo">
+      
+        <h3 className='logo-h3'>food</h3>
+      </div>
 
-                  <li><a className="dropdown-item" href="#">Something else here</a></li>
-                </ul>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link disabled">Link</a>
-              </li>
-            </ul>
-            <form className="d-flex" role="search">
-              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-              <button className="btn btn-outline-success" type="submit">Search</button>
-            </form>
-          </div>
-        </div>
-      </nav>
-    </div>
+      <ul className="classlist">
+        <li>Home</li>
+        <li>About</li>
+      </ul>
+      <div className="drpdown">
+        <div className="drpdown">Food-list</div>
+        
+      </div>
+
+      <div className="search"><input type="text" placeholder='Search here' className='searchbar' />
+      </div>
+      <div className="cart"></div>
+      <div className="button">Login</div>
+      <div className="button">Sign-up</div>
+      <div className="button">Cart <ShoppingBasketIcon /> </div>
+      
+    </nav>
   )
 }
 
