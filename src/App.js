dc9acc76625403a01b3d94fc9e726327e67a6564
Nivedita-Kodli <98208389+Nivedita-Kodli@users.noreply.github.com>
2022-09-26 01:27:41 +0530

    second commit n

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import Header from './component/Header';
-import './App.css';
-import HomePage from './pages/homepage/HomePage';
-import Login from './component/Login';
-import {BrowserRouter, Routes, Route} from "react-router-dom";
-
+import React from "react";
+import Header from "./component/Header";
+import "./App.css";
+import HomePage from "./pages/homepage/HomePage";
+import Login from "./component/Login";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Signup from "./component/Signup";
 
 function App() {
   return (
     <BrowserRouter>
-    <Header/>
-    
-    <Routes>
-    <Route path="/" element={<HomePage/>}></Route>
+      <Header />
 
-      <Route path="login" element={<Login/>}></Route>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Signup />} />
+        <Route path="homePage" element={<HomePage />} />
+        <Route path="login" element={<Login />} />
+      </Routes>
     </BrowserRouter>
   );
 }
--- src/component/Signup.js
+++ src/component/Signup.js
@@ -1,21 +1,20 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Sign_img from "./Sign_img";
-import { useNavigate } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 
-const Login = () => {
-  const history = useNavigate();
+const Signup = () => {
 
   const[inpval, setInpval] = useState({
-    
+    name: "",
     email: "",
-   
+    date: "",
     password: ""
 
   })
   const[data,setData] = useState([])
-  console.log(data)
+  console.log(inpval)
 
   const getdata = (e) => {
       // console.log(e.target.value)
@@ -35,50 +34,47 @@ const Login = () => {
   const addData =(e) => {
     e.preventDefault();
     // console.log(inpval)
-
-    const getuserArr = localStorage.getItem("useryoutube");
-    console.log("getuserArr")
      
-    const {email,password} = inpval;
+    const {name,email,date,password} = inpval;
 
-     if(email === ""){
+    if(name === "") {
+      alert("name field is required")
+    }else if(email === ""){
       alert("email field is required")
     }else if (!email.includes("@")){
       alert("please enter valid email address")
+    }else if(date === ""){
+      alert("date field is required")
     }else if(password === ""){
       alert("password field is required")
     }else if(password.length < 5){
       alert("password length greater than 5" )
     }else{
-       if(getuserArr && getuserArr.length){
-        const userdata = JSON.parse(getuserArr);
-        const userLogin = userdata.filter((el,k)=> {
-          return el.email === email && el.password === password
-        })
-        // console.log(userLogin)
-        if(userLogin.length === 0){
-          alert("invalid details")
-        }else{
-          console.log("user login successfully")
-          history("/homepage")
-        }
-       }
+      console.log("data added successfully")
+
+      localStorage.setItem("useryoutube",JSON.stringify([...data,inpval]));
     }
 
   }
+
+
   return (
     <>
-    <div className="container mt-3">
+      <div className="container mt-3">
         <section  className="d-flex justify-content-between ">
           <div className="left_data mt-3 p-3" style={{width:"100%"}}>
-            <h3 className="text-center col-lg-6">Login</h3>
+            <h3 className="text-center col-lg-6">Sign-Up</h3>
             <Form>
-              
+              <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
+                <Form.Control type="text" onChange={getdata}  name='name' placeholder="Enter Your Name" />
+              </Form.Group>
 
               <Form.Group className="mb-3 col-lg-6" controlId="formBasicPassword">
                 <Form.Control type="email" onChange={getdata} name='email' placeholder="Enter E-mail" />
               </Form.Group>
-              
+              <Form.Group className="mb-3 col-lg-6" controlId="formBasicPassword">
+                <Form.Control type="date"  name='date' onChange={getdata}  />
+              </Form.Group>
               <Form.Group className="mb-3 col-lg-6" controlId="formBasicPassword">
                 <Form.Control type="password" name='password' onChange={getdata} placeholder="Password" />
               </Form.Group>
@@ -87,14 +83,14 @@ const Login = () => {
                 Submit
               </Button>
             </Form>
-            
+            <p className="mt-3">Already Have An Account <span><NavLink to="/login"> Login </NavLink></span></p>
           </div>
 
           <Sign_img />
         </section>
       </div>
     </>
-    
-  )
-}
-export default Login
+  );
+};
+
+export default Signup;
