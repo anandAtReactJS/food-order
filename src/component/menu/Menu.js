bad17213d6d4d160eb85b18e5b6967ccd053278e
anandAtReactJS <anand.sureshh@outlook.com>
2022-09-26 21:55:35 +0530

    Added chineese food

--- src/component/menu/Menu.js
+++ src/component/menu/Menu.js
@@ -8,9 +8,11 @@ import './menu.css';
 const Menu = () => {
     const [burgers, setBurgers] = useState([]);
     const [pizzas,setPizzas]=useState([]);
+    const [noodles,setNoodles]=useState([]);
 
         fetch('http://localhost:3000/food-order/burger.json').then(data => data.json()).then(burger => setBurgers(burger));
-        fetch('http://localhost:3000/food-order/pizza.json').then(data=>data.json()).then(pizza=>setPizzas(pizza))
+        fetch('http://localhost:3000/food-order/pizza.json').then(data=>data.json()).then(pizza=>setPizzas(pizza));
+        fetch('http://localhost:3000/food-order/Noodles.json').then(data=>data.json()).then(noodle=>setNoodles(noodle));
 
     
 
@@ -53,6 +55,29 @@ const Menu = () => {
                                         <p className="card-text">{pizza.desc}</p>
                                         <p>{pizza.time}| Serves: 1</p>
                                         <span>${pizza.price} <del>$10.70</del></span>
+                                        <a href="#" className='btn btn-success'>Add to cart</a>
+                                    </div>
+                                </div>
+                            </div>
+
+                        ))}
+                    </div>
+                </div>
+            </section>
+
+            <section className="bg-light py-4 my-5">
+                <div className="container">
+                    <div className="row">
+                        <h2 style={{ color: 'black', fontFamily: 'cursive', fontStyle: 'italic', textAlign: 'center' }}>Chinese Option's</h2>
+                        {noodles.map(noodle => (
+                            <div id={noodles.id} className="col-md-6 col-lg-4 burger">
+                                <div className="card my-3">
+                                    <img src={noodle.pimg} className="img-fluid" alt="thumbnail" />
+                                    <div className="card-body">
+                                        <h4 className="card-title">{noodle.name}</h4>
+                                        <p className="card-text">{noodle.desc}</p>
+                                        <p>{noodle.time}| Serves: 1</p>
+                                        <span>${noodle.price} <del>$10.70</del></span>
                                         <a href="#" className='btn btn-success'>Add to cart</a>
                                     </div>
                                 </div>
