65606a21143caace40d971ce8e54f748f03e5469
Nivedita-Kodli <98208389+Nivedita-Kodli@users.noreply.github.com>
2022-09-27 12:10:55 +0530

    third commit n

--- src/App.js
+++ src/App.js
@@ -5,16 +5,20 @@ import HomePage from "./pages/homepage/HomePage";
 import Login from "./component/Login";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Signup from "./component/Signup";
+import Cart from "./component/Cart";
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
-
+      
       <Routes>
-        <Route path="/" element={<Signup />} />
-        <Route path="homePage" element={<HomePage />} />
+        <Route path="/food-order" element={<Signup />} />
         <Route path="login" element={<Login />} />
+        <Route path="homePage" element={<HomePage />} />
+        <Route path="cart" element={<Cart />} />
+        <Route path="menu" element={<Menu/>} />
+
       </Routes>
     </BrowserRouter>
   );
--- src/component/Header.js
+++ src/component/Header.js
@@ -1,49 +1,49 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
-import React from 'react';
-import Form from 'react-bootstrap/Form';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Foodies from "../component/foodie.png";
+import { Badge } from "@material-ui/core";
 
-function Header() {
+function Header({}) {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand href="#home">foodies</Navbar.Brand>
+        {/* <Foodies/> */}
+        <img src={Foodies} alt="" style={{ height: "58.16px" }} />
+        {/* <Navbar.Brand href="#home">Foodies</Navbar.Brand> */}
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/homePage">home</Nav.Link>
-            <Nav.Link href="/about">About</Nav.Link>
-            <NavDropdown title="Food-List" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Burger</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Pizza</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Chinese</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.4">Coffee</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.5">Shakes</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.6">Pure-veg</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.7">Non-veg</NavDropdown.Item>
-              
-              
-            </NavDropdown>
+            <Nav.Link href="/homePage">Home</Nav.Link>
+            <Nav.Link href="/about">About Us</Nav.Link>
+            <Nav.Link href="/menu">Menu</Nav.Link>
           </Nav>
-          <Nav>
-          <Form className="d-flex">
-                  <Form.Control
-                    type="search"
-                    placeholder="Search"
-                    className="me-2"
-                    aria-label="Search"
-                  />
-                  
-                </Form>
 
+          <Nav>
+            <Form className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+                // onChange={handleFilter}
+              />
+            </Form>
           </Nav>
-          <Nav className='justify-content-between'>
+          <Nav className="justify-content-between">
             <Nav.Link href="/login">Login</Nav.Link>
-            <Nav.Link eventKey={2} href="/" mr-4>Sign-Up</Nav.Link>
-            <Nav.Link eventKey={3} href="/cart">Cart <ShoppingBasketIcon/></Nav.Link>
+            <Nav.Link eventKey={2} href="/food-order" mr-4>
+              Sign-Up
+            </Nav.Link>
+            <Nav.Link eventKey={3} href="/cart">
+              Cart
+              <Badge badgeContent={4} color="primary">
+                <ShoppingBasketIcon />
+              </Badge>
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
